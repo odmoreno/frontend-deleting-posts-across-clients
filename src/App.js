087899cd330af0e1c4e52c0af3a72d6ab6431304
Oscar Moreno <oscar.moreno@cti.espol.edu.ec>
2024-08-21 16:15:14 -0500

    uploading image, login, update, created posts

--- src/App.js
+++ src/App.js
@@ -58,52 +58,62 @@ class App extends Component {
 
   loginHandler = (event, authData) => {
     event.preventDefault();
-    this.setState({ authLoading: true });
-    fetch("http://localhost:8080/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: authData.email,
-        password: authData.password,
-      }),
-    })
-      .then((res) => {
-        if (res.status === 422) {
-          throw new Error("Validation failed.");
+    const graphqlQuery = {
+			query: `
+      {
+        login(email: "${authData.email}", password: "${authData.password}"){
+          token
+          userId
         }
-        if (res.status !== 200 && res.status !== 201) {
-          console.log("Error!");
-          throw new Error("Could not authenticate you!");
-        }
-        return res.json();
-      })
-      .then((resData) => {
-        console.log(resData);
-        this.setState({
-          isAuth: true,
-          token: resData.token,
-          authLoading: false,
-          userId: resData.userId,
-        });
-        localStorage.setItem("token", resData.token);
-        localStorage.setItem("userId", resData.userId);
-        const remainingMilliseconds = 60 * 60 * 1000;
-        const expiryDate = new Date(
-          new Date().getTime() + remainingMilliseconds
-        );
-        localStorage.setItem("expiryDate", expiryDate.toISOString());
-        this.setAutoLogout(remainingMilliseconds);
-      })
-      .catch((err) => {
-        console.log(err);
-        this.setState({
-          isAuth: false,
-          authLoading: false,
-          error: err,
-        });
-      });
+      }
+      `,
+		}
+		console.log(authData)
+		this.setState({ authLoading: true })
+		fetch("http://localhost:8080/graphql", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(graphqlQuery),
+		})
+			.then((res) => {
+				return res.json()
+			})
+			.then((resData) => {
+				if (resData.errors && resData.errors[0].status === 422) {
+					throw new Error(
+						"Validation failed. Make sure the email address isn't used yet!"
+					)
+				}
+				if (resData.errors) {
+					console.log(resData.errors)
+					throw new Error("User login failed")
+				}
+				console.log(resData)
+				this.setState({
+					isAuth: true,
+					token: resData.data.login.token,
+					authLoading: false,
+					userId: resData.data.login.userId,
+				})
+				localStorage.setItem("token", resData.data.login.token)
+				localStorage.setItem("userId", resData.data.login.userId)
+				const remainingMilliseconds = 60 * 60 * 1000
+				const expiryDate = new Date(
+					new Date().getTime() + remainingMilliseconds
+				)
+				localStorage.setItem("expiryDate", expiryDate.toISOString())
+				this.setAutoLogout(remainingMilliseconds)
+			})
+			.catch((err) => {
+				console.log(err)
+				this.setState({
+					isAuth: false,
+					authLoading: false,
+					error: err,
+				})
+			})
   };
 
   signupHandler = (event, authData) => {
